3c74cc926b9863267fac7ac0ae3676f9163dadd9
zzqnike <zzqnike@gmail.com>
2024-05-23 22:53:13 +0800

    od29:寻找素数对 update

--- nowcoder/od/29.cpp
+++ nowcoder/od/29.cpp
@@ -6,28 +6,6 @@
 #include <vector>
 
 using namespace std;
-
-/**
- * 27:解密犯罪时间
- * OJ：100%
- * 警察在侦破一个案件时，得到了线人给出的可能犯罪时间，形如“HH:MM”表示的时刻。
- * 根据警察和线人的约定，为了隐蔽，该时间是修改过的解密规则为:
- * 利用当前出现过的数字，构造下一个距离
- * 当前时间只最近的时刻，则该时间为可能的犯罪时间。
- * 每个出现数字都可以被无限次使用。
- * 输入：形如HH:MM的字符串
- * 输出：
- * 推理出的可能的犯罪时间
- */
-#include <climits>
-#include <iostream>
-#include <set>
-#include <sstream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
 /**
  * Honor
  * 寻找素数对
@@ -61,20 +39,11 @@ int main() {
         cout << 0 << endl;
         return 0;
     }
-    int num = 51;
-    //    cout << isPrime(num) << endl;
-    //    for (int i = 1; i <= 223m / 2; ++i) {
-    //        if (isPrime(i) && isPrime(m - i)) {
-    //            cout << i << " " << m - i << endl;
-    //        }
-    //    }
-
     for (int i = m / 2; i >= 1; --i) {
         if (isPrime(i) && isPrime(m - i)) {
             cout << i << " " << m - i << endl;
             break;
         }
     }
-
     return 0;
 }
