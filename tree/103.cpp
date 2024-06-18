926acda9fa0e48543643a3d8c8891c1a5d1e0c40
zzqnike <zzqnike@gmail.com>
2024-06-19 00:00:13 +0800

    103:二叉树的锯齿形层序遍历

--- tree/103.cpp
+++ tree/103.cpp
@@ -20,32 +20,40 @@
 using namespace std;
 
 /**
- * 107：二叉树的层序遍历 II
- * 给你二叉树的根节点 root ，返回其节点值 自底向上的层序遍历 。
- * （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ * 103：二叉树的锯齿形层序遍历
+ * 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。
+ * （即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
  *
- * https://leetcode.cn/problems/binary-tree-level-order-traversal-ii/description/
+ * https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal/description/
  */
 class Solution {
    public:
     /**
-     * BFS 中序遍历 102的结果逆序
+     * BFS 中序遍历
+     * 双端队列存储 每层数据。
+     * 一层添加到队列头，下一层添加到队列尾
      */
-    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
         if (root == nullptr) return ans;
         queue<TreeNode*> queue;
         queue.push(root);
+        bool reverse = false;
         while (!queue.empty()) {
             // 当前层的结点个数
             int levelSize = queue.size();
-            cout << levelSize << endl;
-            vector<int> list;
+            // cout << levelSize << endl;
+            deque<int> list;
             // 控制弹出当前层的所有元素
             for (int i = 0; i < levelSize; i++) {  // 循环 size 次
                 TreeNode* front = queue.front();
                 queue.pop();
-                list.push_back(front->val);
+                if (reverse) {
+                    list.push_front(front->val);
+                } else {
+                    list.push_back(front->val);
+                }
+
                 if (front->left) {
                     queue.push(front->left);
                 }
@@ -53,10 +61,9 @@ class Solution {
                     queue.push(front->right);
                 }
             }
-            ans.push_back(list);
+            reverse = !reverse;
+            ans.push_back(vector<int>(list.begin(), list.end()));
         }
-        // 逆转数组
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
@@ -67,7 +74,7 @@ int main() {
     vector<vector<int>> arr = {
         {3, 9, 20, null, null, 15, 7}, {3, 9, 20, 10, 11, 15, 7}, {1, null, 2}};
     for (auto& i : arr) {
-        vector<vector<int>> ans = s->levelOrderBottom(new TreeNode(i));
+        vector<vector<int>> ans = s->zigzagLevelOrder(new TreeNode(i));
         for (auto& j : ans) {
             cout << "[";
             for (auto& k : j) {
