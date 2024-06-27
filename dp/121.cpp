2e63263cf27f01de361e1c3556ef17c7c2a4a0aa
zzqnike <zzqnike@gmail.com>
2024-06-27 18:50:52 +0800

    121:买卖股票的最佳时机

--- dp/121.cpp
+++ dp/121.cpp
@@ -23,7 +23,7 @@ using namespace std;
  * 天的价格。 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子
  * 卖出该股票。设计一个算法来计算你所能获取的最大利润。
  * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
- * https://leetcode.cn/problems/unique-paths/description/
+ * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/description/
  */
 class Solution {
    public:
@@ -76,10 +76,67 @@ class Solution {
     }
 
     /**
-     * 法3：一次遍历，时间：O(n), 空间 O(1)
-     * 找到最低价格买入，在后面的最高价出卖出
+     * 动态规划 O(n) O(n)
      */
     int maxProfit3(vector<int>& prices) {
+        int n = prices.size();
+        if (n <= 1) return 0;
+        // dp[i][0] 第i天交易结束后持有股票的利润
+        // dp[i][1] 第i天交易结束后不持有股票的利润
+        vector<vector<int>> dp(n, vector<int>(2));
+        // 初始化
+        dp[0][0] = -prices[0];  // 第一天买入后利润为负的
+        dp[0][1] = 0;
+        // 状态转移
+        for (int i = 1; i < n; i++) {
+            // 第 i 天持有，i - 1天就持有，或者第 i 天买入
+            dp[i][0] = max(dp[i - 1][0], -prices[i]);
+
+            // 第 i 天不持有 i - 1天就不持有，
+            // 或者第 i 天卖出 = i - 1天持有时 + i 天的价格
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                cout << dp[i][j] << " ";
+            }
+            cout << endl;
+        }
+        // 肯定是不持有多，即卖出
+        return dp[n - 1][1];
+    }
+
+    /**
+     * 动态规划压缩空间 O(n) O(1)
+     */
+    int maxProfit4(vector<int>& prices) {
+        int n = prices.size();
+        if (n <= 1) return 0;
+        // dp[i][0] 第i天交易结束后持有股票的利润
+        // dp[i][1] 第i天交易结束后不持有股票的利润
+        // 初始化
+        int hold = -prices[0];  // 第一天买入后利润为负的
+        int nonHeld = 0;
+        // 状态转移
+        for (int i = 1; i < n; i++) {
+            // 第 i 天持有，i - 1天就持有，或者第 i 天买入
+            int lastHold = hold;
+            hold = max(hold, -prices[i]);
+
+            // 第 i 天不持有 i - 1天就不持有，
+            // 或者第 i 天卖出 = i - 1天持有时 + i 天的价格
+            nonHeld = max(nonHeld, lastHold + prices[i]);
+        }
+        // 肯定是不持有多，即卖出
+        return nonHeld;
+    }
+
+    /**
+     * 法3：贪心思想 一次遍历，时间：O(n), 空间 O(1)
+     * 找到最低价格买入，在后面的最高价出卖出
+     */
+    int maxProfit5(vector<int>& prices) {
         int minPrice = INT_MAX;
         int maxProfit = 0;
         for (int i = 0; i < prices.size(); i++) {
@@ -100,7 +157,7 @@ int main() {
     Solution* s = new Solution();
     vector<vector<int>> list = {{7, 1, 5, 3, 6, 4}, {7, 6, 4, 3, 1}};
     for (auto i : list) {
-        int count = s->maxProfit3(i);
+        int count = s->maxProfit4(i);
         cout << count << endl;
     }
 }
