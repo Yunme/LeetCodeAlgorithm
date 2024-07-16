045fe28ba9ed1771b452771961112831e6483d92
zzqnike <zzqnike@gmail.com>
2024-07-16 13:38:08 +0800

    77:组合update

--- backtrack/77.cpp
+++ backtrack/77.cpp
@@ -31,6 +31,7 @@ class Solution {
      */
     void backtracking(int n, int k, int index, vector<int> &temp,
                       vector<vector<int>> &ans) {
+        cout << index << " ";
         // 收集到了 k 个 添加到结果集
         if (temp.size() == k) {
             ans.push_back(temp);
@@ -51,15 +52,44 @@ class Solution {
         vector<vector<int>> ans;
         // 每个组合
         vector<int> temp;
-        backtracking(n, k, 1, temp, ans);
+        backtracking2(n, k, 1, temp, ans);
         return ans;
+    }
+
+    /**
+     * 剪枝
+     */
+    void backtracking2(int n, int k, int index, vector<int> &temp,
+                       vector<vector<int>> &ans) {
+        cout << index << " ";
+        // 收集到了 k 个 添加到结果集
+        if (temp.size() == k) {
+            ans.push_back(temp);
+            return;
+        }
+        // 做选择，从当前向后选择
+        // 如果当前待选择个数小于目标个数，则直接返回
+        int size = temp.size();  // 当前已选择个数
+        int remain = k - size;   // 还需要选择的个数
+        // 剩余可选的：n - index + 1
+        // 剩余可选的 小于 还需要选择的 直接return
+        if (n - index + 1 < remain) return;
+        // 或 i <= n - (k - size) + 1
+        for (int i = index; i <= n; i++) {
+            // 当前值放入
+            temp.push_back(i);
+            // 下一个值
+            backtracking2(n, k, i + 1, temp, ans);
+            // 回溯
+            temp.pop_back();
+        }
     }
 };
 
 int main() {
     Solution *s = new Solution();
-    vector<int> n = {4, 1};
-    vector<int> k = {2, 1};
+    vector<int> n = {4, 1, 4};
+    vector<int> k = {2, 1, 4};
 
     for (int i = 0; i < n.size(); i++) {
         auto ans = s->combine(n[i], k[i]);
